a55d7607cc103c1d7919ecf44ebddf20d5e1c425
Henrik Gerdes <hegerdes@uni-osnabrueck.de>
2021-05-01 00:23:13 +0200

    Now use b-container and rows

--- pages/index.vue
+++ pages/index.vue
@@ -5,99 +5,101 @@
         @update-data="refresh()"
       />
     </div>
-    <div class="container">
-      <div id="dropdown-search">
-        <b-dropdown :text="searchType.text" class="mb-2 mr-sm-4 mb-sm-0">
-          <b-dropdown-item v-for="query in querys" :key="query.text" @click="searchSelected(query)">
-            {{ query.text }}
-            <b-dropdown-divider />
-          </b-dropdown-item>
-        </b-dropdown>
-      </div>
-      <div id="session-search-options">
-        <b-form v-if="searchType.id === 1" inline @submit.stop.prevent>
-          <label class="sr-only" for="inline-form-input-start-date">Start Date</label>
-          <b-form-datepicker
-            id="start-datepicker"
-            v-model="startDate"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            :date-disabled-fn="dateDisabled"
-            locale="de"
-            placeholder="Start Date"
-          />
-          <label class="sr-only" for="inline-form-input-end-date">End Date</label>
-          <b-form-datepicker
-            id="end-datepicker"
-            v-model="endDate"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            :date-disabled-fn="dateDisabled"
-            locale="de"
-            placeholder="End Date"
-          />
-          <b-form-text id="input-session-info" class="mb-2 mr-sm-2 mb-sm-0">
-            Or
-          </b-form-text>
-          <label class="sr-only" for="inline-form-input-session">SessionSearch</label>
-          <b-form-input
-            id="session-search-in"
-            v-model="sessionIndices"
-            name="session-imput-1"
-            :state="sessionState('name')"
-            aria-describedby="input-live-feedback"
-            placeholder="SessionIDs"
-            trim
-          />
-          <!-- This will only be shown if the preceding input has an invalid state -->
-          <b-form-invalid-feedback id="input-live-feedback">
-            Enter at least 3 letters
-          </b-form-invalid-feedback>
-        </b-form>
-        <b-form v-if="searchType.id === 2" inline @submit.stop.prevent>
-          <label class="sr-only" for="inline-form-input-start-date">Start Date</label>
-          <b-form-datepicker
-            id="session-datepicker"
-            v-model="startDate"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            :date-disabled-fn="dateDisabled"
-            locale="de"
-            placeholder="Date"
-          />
-          <b-form-text id="input-session-info" class="mb-2 mr-sm-2 mb-sm-0">
-            Or
-          </b-form-text>
-          <label class="sr-only" for="inline-form-input-session">SessionSearch</label>
-          <b-form-input
-            id="session-search-in"
-            v-model="sessionIndices"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            name="session-imput-1"
-            :state="sessionState('name')"
-            aria-describedby="input-live-feedback"
-            placeholder="SessionID"
-            trim
-          />
-          <b-form-text id="input-session-info" class="mb-2 mr-sm-2 mb-sm-0">
-            Or
-          </b-form-text>
-          <label class="sr-only" for="inline-form-input-session">SessionSearch</label>
-          <b-form-input
-            id="mp-search-in"
-            v-model="mpName"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            name="mp-imput-1"
-            :state="mpState('name')"
-            aria-describedby="input-live-feedback"
-            placeholder="MP name or ID"
-            trim
-          />
+    <b-container fluid>
+      <b-form-row align="left" class="top-buffer">
+        <b-col id="dropdown-search" sm="2">
+          <b-dropdown :text="searchType.text" class="mb-2 mr-sm-4 mb-sm-0">
+            <b-dropdown-item v-for="query in querys" :key="query.id" @click="searchSelected(query)">
+              {{ query.text }}
+              <b-dropdown-divider />
+            </b-dropdown-item>
+          </b-dropdown>
+        </b-col>
+        <b-col id="session-search-options" sm="10">
+          <b-form v-if="searchType.id === 1" inline @submit.stop.prevent>
+            <label class="sr-only" for="inline-form-input-start-date">Start Date</label>
+            <b-form-datepicker
+              id="start-datepicker"
+              v-model="startDate"
+              class="mb-2 mr-sm-2 mb-sm-0"
+              :date-disabled-fn="dateDisabled"
+              locale="de"
+              placeholder="Start Date"
+            />
+            <label class="sr-only" for="inline-form-input-end-date">End Date</label>
+            <b-form-datepicker
+              id="end-datepicker"
+              v-model="endDate"
+              class="mb-2 mr-sm-2 mb-sm-0"
+              :date-disabled-fn="dateDisabled"
+              locale="de"
+              placeholder="End Date"
+            />
+            <b-form-text id="input-session-info" class="mb-2 mr-sm-2 mb-sm-0">
+              Or
+            </b-form-text>
+            <label class="sr-only" for="inline-form-input-session">SessionSearch</label>
+            <b-form-input
+              id="session-search-in"
+              v-model="sessionIndices"
+              name="session-imput-1"
+              :state="sessionState('name')"
+              aria-describedby="input-live-feedback"
+              placeholder="SessionIDs"
+              trim
+            />
+            <!-- This will only be shown if the preceding input has an invalid state -->
+            <b-form-invalid-feedback id="input-live-feedback">
+              Enter at least 3 letters
+            </b-form-invalid-feedback>
+          </b-form>
+          <b-form v-if="searchType.id === 2" inline @submit.stop.prevent>
+            <label class="sr-only" for="inline-form-input-start-date">Start Date</label>
+            <b-form-datepicker
+              id="session-datepicker"
+              v-model="startDate"
+              class="mb-2 mr-sm-2 mb-sm-0"
+              :date-disabled-fn="dateDisabled"
+              locale="de"
+              placeholder="Date"
+            />
+            <b-form-text id="input-session-info" class="mb-2 mr-sm-2 mb-sm-0">
+              Or
+            </b-form-text>
+            <label class="sr-only" for="inline-form-input-session">SessionSearch</label>
+            <b-form-input
+              id="session-search-in"
+              v-model="sessionIndices"
+              class="mb-2 mr-sm-2 mb-sm-0"
+              name="session-imput-1"
+              :state="sessionState('name')"
+              aria-describedby="input-live-feedback"
+              placeholder="SessionID"
+              trim
+            />
+            <b-form-text id="input-session-info" class="mb-2 mr-sm-2 mb-sm-0">
+              Or
+            </b-form-text>
+            <label class="sr-only" for="inline-form-input-session">SessionSearch</label>
+            <b-form-input
+              id="mp-search-in"
+              v-model="mpName"
+              class="mb-2 mr-sm-2 mb-sm-0"
+              name="mp-imput-1"
+              :state="mpState('name')"
+              aria-describedby="input-live-feedback"
+              placeholder="MP name or ID"
+              trim
+            />
 
-          <!-- This will only be shown if the preceding input has an invalid state -->
-          <b-form-invalid-feedback id="input-live-feedback">
-            Enter at least 3 letters
-          </b-form-invalid-feedback>
-        </b-form>
-      </div>
-    </div>
+            <!-- This will only be shown if the preceding input has an invalid state -->
+            <b-form-invalid-feedback id="input-live-feedback">
+              Enter at least 3 letters
+            </b-form-invalid-feedback>
+          </b-form>
+        </b-col>
+      </b-form-row>
+    </b-container>
   </div>
 </template>
 
@@ -166,6 +168,8 @@ export default Vue.extend({
   align-items: center;
   text-align: center;
 }
+
+.top-buffer { margin-top:20px; }
 
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
